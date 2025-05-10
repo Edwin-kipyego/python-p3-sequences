02d5b252c7ad94d6cc511b8cf95a6b897383e99c
Edwin kipyego <koriredwin95@gmail.com>
2025-05-10 11:28:12 +0300

    feat: sequence complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+    elif length == 1:
+        print("[0]")
+    elif length == 2:
+        print("[0, 1]")
+    else:
+        fib_list = [0, 1]
+        for _ in range(2, length):  
+            next_number = fib_list[-1] + fib_list[-2]
+            fib_list.append(next_number)
+        print(f"{fib_list}")
